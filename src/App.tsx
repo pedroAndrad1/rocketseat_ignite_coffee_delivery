4ca2b16f9e204851ae09937c3ddaeeecce59588b
Pedro de Andrade <dev.pedroandrade@gmail.com>
2022-12-02 20:59:18 -0300

    tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import {ThemeProvider} from 'styled-components';
+import {theme} from './styles/theme';
 function App() {
  
   return (
-    <div>
-
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        
+      </ThemeProvider>
+    </>
   )
 }
 
